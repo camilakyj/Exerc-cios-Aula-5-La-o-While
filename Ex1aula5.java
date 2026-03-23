c2a154ac304db251d569130bc4fbc542eb0f3045
Camila <yagimaj@gmail.com>
2026-03-23 18:58:32 -0300

    Add files via upload

---
+++ Ex1aula5.java
@@ -0,0 +1,17 @@
+// Camila Aki Yagima Jeronimo
+package dobackofront;
+
+public class Ex1aula5 {
+    public static void main (String[] args) {
+        int contador;
+        contador = 100;
+
+        while (contador <= 200) {
+            System.out.print(contador + " ");
+            contador++;
+
+        }
+        }
+
+
+    }

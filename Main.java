b547739b6232c88610ffed4ec77576e1d3b772a4
Camila <yagimaj@gmail.com>
2026-03-24 16:44:09 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+// Camila Aki Yagima Jeronimo Ex2 Aula5 
+package dobackofront;
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int numero;
+        int pares = 0;
+        int impares = 0;
+
+        for (int i = 1; i <=10; i++) {
+            System.out.print("Digite um numero inteiro:");
+            numero = entrada.nextInt();
+
+            if (numero % 2 == 0) {
+                pares++;
+
+            } else {
+                impares++;
+            }
+        }
+
+        System.out.println("Quantidade de numeros pares:" + pares);
+        System.out.println("Quantidade de numeros impares: " + impares);
+
+
+        entrada.close();
+
+
+
+    }}
